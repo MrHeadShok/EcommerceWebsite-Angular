a96e353b3f38b034ff88b412148ed09c05a0e97e
Taieb Belkahla <54212105+Taieb-Belkahla@users.noreply.github.com>
2021-04-03 22:02:22 +0200

    adding existing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ShoppingcartComponent } from './components/shoppingcart/shoppingcart.component';
+import { FiltersComponent } from './components/shoppingcart/filters/filters.component';
+import { ProductlistComponent } from './components/shoppingcart/productlist/productlist.component';
+import { CartComponent } from './components/shoppingcart/cart/cart.component';
+import { CartItemComponent } from './components/shoppingcart/cart/cart-item/cart-item.component';
+import { ProductitemComponent } from './components/shoppingcart/productlist/productitem/productitem.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ShoppingcartComponent,
+    NavComponent,
+    FiltersComponent,
+    ProductlistComponent,
+    CartComponent,
+    CartItemComponent,
+    ProductitemComponent
   ],
   imports: [
     BrowserModule,
